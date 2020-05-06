caee93bb385994970cc051be3ac5450be6672d43
raulhechavarria <raulh@DESKTOP-S9QKGHR>
2020-05-06 16:21:47 -0400

    every mundo

--- customer-services/src/main/java/com/customer/configuration/QueueProducer.java
+++ customer-services/src/main/java/com/customer/configuration/QueueProducer.java
@@ -7,7 +7,6 @@ import ch.qos.logback.classic.Logger;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
